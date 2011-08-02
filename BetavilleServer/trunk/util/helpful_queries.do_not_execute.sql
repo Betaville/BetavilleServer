5b3b0f7c9950f92a01a3782924cc1ed08a7d2374
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2011-08-02 14:03:04 +0000

    Added more queries to the helpful queries script

--- BetavilleServer/trunk/util/helpful_queries.do_not_execute.sql
+++ BetavilleServer/trunk/util/helpful_queries.do_not_execute.sql
@@ -8,3 +8,9 @@ SELECT * FROM design LEFT JOIN proposal ON design.designid=proposal.destinationi
 
 -- Delete all of the base designs in a city
 UPDATE design LEFT JOIN proposal ON design.designid=proposal.destinationid set isalive=0 WHERE proposal.destinationid IS NULL AND cityid=2;
+
+-- Count the number of non-deleted designs in the database --
+SELECT count(*) FROM design WHERE isalive=1;
+
+-- Count the number of non-deleted proposals in the database --
+SELECT count(*) FROM design LEFT JOIN proposal ON design.designid=proposal.destinationid WHERE proposal.destinationid IS NOT NULL AND isalive=1;
