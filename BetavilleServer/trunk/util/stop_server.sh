21f6011dfac2a681822790b148d252a3198569a0
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2012-01-18 05:55:19 +0000

    Added pid file support to Betaville Server

--- BetavilleServer/trunk/util/stop_server.sh
+++ BetavilleServer/trunk/util/stop_server.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # A simple script for turning off the Betaville server software
-# Copyright 2011 Brooklyn eXperimental Media Center
+# Copyright 2011-2012 Brooklyn eXperimental Media Center
 # Released under BSD 3-Clause license
 # author: Skye Book <skye.book@gmail.com>
 
-PID=$(ps aux | egrep [B]etavilleServer | awk '{print $ 2}')
-kill $PID
+kill pid < cat BetavilleServer.lock
